33e28c13c72eb1ed7853a0384239af7e10828c7e
r.polosmak <r.polosmak@newlinetechnologies.net>
2020-05-14 15:50:34 +0300

    add flyway database migration

--- src/main/java/ua/polosmak/controller/UserController.java
+++ src/main/java/ua/polosmak/controller/UserController.java
@@ -1,6 +1,7 @@
 package ua.polosmak.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,30 +11,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ua.polosmak.domain.Role;
 import ua.polosmak.domain.User;
-import ua.polosmak.repository.UserRepository;
+import ua.polosmak.service.UserService;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAuthority('ADMIN')")
-public class UserController {
-	private final UserRepository userRepo;
 
-	public UserController(UserRepository userRepo) {
-		this.userRepo = userRepo;
+public class UserController {
+	private final UserService userService;
+
+	public UserController(UserService userService) {
+		this.userService = userService;
 	}
 
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@GetMapping
 	public String userList(Model model) {
-		model.addAttribute("users", userRepo.findAll());
+		model.addAttribute("users", userService.findAll());
 
 		return "userList";
 	}
 
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@GetMapping("{user}")
 	public String userEditForm(@PathVariable User user, Model model) {
 		model.addAttribute("user", user);
@@ -42,28 +42,31 @@ public class UserController {
 		return "userEdit";
 	}
 
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@PostMapping
 	public String userSave(
 			@RequestParam String username,
 			@RequestParam Map<String, String> form,
 			@RequestParam("userId") User user
 	) {
-		user.setUsername(username);
-
-		Set<String> roles = Arrays.stream(Role.values())
-				.map(Role::name)
-				.collect(Collectors.toSet());
-
-		user.getRoles().clear();
-
-		for (String key : form.keySet()) {
-			if (roles.contains(key)) {
-				user.getRoles().add(Role.valueOf(key));
-			}
-		}
-
-		userRepo.save(user);
+		userService.saveUser(user, username, form);
 
 		return "redirect:/user";
+	}
+
+	@GetMapping("profile")
+	public String getProfile(Model model, @AuthenticationPrincipal User user) {
+		model.addAttribute("username", user.getUsername());
+		model.addAttribute("email", user.getEmail());
+
+		return "profile";
+	}
+
+	@PostMapping("/profile")
+	public String uodateProfile(@AuthenticationPrincipal User user,
+								@RequestParam String password,
+								@RequestParam String email) {
+		userService.updateProfile(user, password, email);
+		return"redirect:user/profile";
 	}
 }
--- src/main/java/ua/polosmak/domain/Message.java
+++ src/main/java/ua/polosmak/domain/Message.java
@@ -12,7 +12,7 @@ import javax.persistence.ManyToOne;
 public class Message {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer id;
+	private Long id;
 
 	private String text;
 	private String tag;
@@ -52,11 +52,11 @@ public class Message {
 		return text;
 	}
 
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
